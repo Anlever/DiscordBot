6b02638d8ea0fabb43ef2f09c0d888712715c2b2
Anlever <pushok20125@gmail.com>
2024-06-27 23:24:41 +0300

    First commit

---
+++ Bot.py
@@ -0,0 +1,62 @@
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from threading import Thread
+
+DISCORD_BOT_TOKEN = "MTI1MjIyNDk1NTU0Njk5Njg1MQ.Gz7yq9.IuJeCubSMYTumB2RiX_Bo-WHYgHZ8m73NbL8Ro"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    t1 = Thread(target=write_message)
+    t1.start()
+    
+
+@client.event
+async def on_message(message):
+    if message.author.id == 249133834866655232:
+        emoji = '🤡'
+        await message.add_reaction(emoji)
+    if message.author.id == 272345981142695936:
+        emoji = '💤'
+        await message.add_reaction(emoji)
+    if message.author.id == 151662277001609216:
+        emoji = '🚼'
+        await message.add_reaction(emoji)
+    if message.author.id == 205998644262862849:
+        await message.channel.send("О привет Рома")
+    if message.author.id == 241517599165382656:
+        await message.channel.send("Тест")
+
+
+def write_message():
+    while True:
+        target_channel = client.get_channel(int(1067805711049949275))
+        
+        print("Id чела для упоминания")
+        user_id = str(input())
+        print("Пиши сообщение")
+        message = str(input())
+        
+        
+        
+        if (user_id == ""):
+            asyncio.run_coroutine_threadsafe(target_channel.send(f"{message}"), client.loop)
+        else:
+            asyncio.run_coroutine_threadsafe(target_channel.send(f"<@{user_id}> {message}"), client.loop)
+            
+        
+        
+        
+    
+    
+    
+    
+    
+
+client.run(DISCORD_BOT_TOKEN)
